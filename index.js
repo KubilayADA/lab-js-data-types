3ce23ecfee1497793b0ab9384baec7a61003ec46
Kubilay ADA <kubilayada.26@gmail.com>
2024-06-05 13:12:01 +0200

    Solved Lab

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let result = (s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " ");
 
 // Print out the concatenated string
 
-
+console.log(result);
 
 
 /*******************************************
@@ -22,10 +22,21 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+let result1= "";
+let result2= "";
+if (part1.length > 0){
+    let lastCharacterUpperCase = part1.charAt(part1.length-1).toUpperCase();
+    let restOfString= part1.slice(0, -1);
+    result1 = (restOfString + lastCharacterUpperCase);
+}
+if (part2.length > 0) {
+    let lastCharacterUpperCase = part2.charAt(part2.length-1).toUpperCase();
+    let restOfString = part2.slice(0, -1);
+    result2 = (restOfString + lastCharacterUpperCase);
+}
+let cameLtaiL= (result1 + result2);
 // Print the cameLtaiL-formatted string
-
+    console.log(cameLtaiL);
 
 
 
@@ -35,10 +46,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tip = (84 * 15) / 100;
 
 // Print out the tipAmount
-
+console.log(tip);
 
 
 
@@ -47,10 +58,15 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+function getRandomInt(min, max){
+    max = Math.ceil(max);
+    min = Math.floor(min);
+    return Math.floor(Math.random() * (max - min + 1) +min);
+}
+let randomInt = getRandomInt(0,10);
 
 // Print the generated random number
-
+console.log(randomInt);
 
 
 /*******************************************
@@ -62,15 +78,23 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+false
 const expression2 = a || b;
-
+true
 const expression3 = !a && b;
-
+false
 const expression4 = !(a && b);
-
+true
 const expression5 = !a || !b;
-
+true
 const expression6 = !(a || b);
-
+false
 const expression7 = a && a;
+true
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
